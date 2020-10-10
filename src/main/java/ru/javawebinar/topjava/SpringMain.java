888e127b89977c8a6f5b19b593faee31de3ce369
Roma <roma.kishinka@yandex.ru>
2020-10-10 14:01:50 +0300

    commit sout

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -17,6 +17,8 @@ public class SpringMain {
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
         }
 
+        System.out.println("ht2");
+
     }
 
 
